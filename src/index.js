508e30a793f08816f6f048a7616f1b678007811c
Dibyajyoti Sahoo <dibyajyotisahoo2504@gmail.com>
2024-03-19 23:06:03 +0530

    used express to listen on port

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { port } from "./constants.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error: ", error);
+      throw error;
+    });
+
+    app.listen(port, () => {
+      console.log(`Server is running at port: ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection Failed: ", error);
+  });
