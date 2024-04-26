cb5c29f56a2751aaa6a0b3d57f6b7a808a86bdd6
Dibyajyoti Sahoo <dibyajyotisahoo2504@gmail.com>
2024-04-26 22:07:04 +0530

    add video router

--- src/app.js
+++ src/app.js
@@ -22,9 +22,11 @@ app.use(cookieParser());
 
 // routes import
 import userRouter from "./routes/user.routes.js";
+import videoRouter from "./routes/video.routes.js";
 
 // routes declaration
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/videos", videoRouter);
 
 // how it will see: http://localhost:8000/api/v1/users/register
 
