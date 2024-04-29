6e4ff4b34311642d59db4a57661f51c63cd75120
Dibyajyoti Sahoo <dibyajyotisahoo2504@gmail.com>
2024-04-29 21:32:06 +0530

    create getSubscribedChannels enpoint

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -75,8 +75,80 @@ const getUserChannelSubscribers = asyncHandler(async (req, res) => {
   ]);
 
   // Validate the subscriberList
-  if (!subscribersList?.length) {
+  if (!subscribersList) {
     throw new ApiError(500, "Subscriber list is not found");
+  }
+
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(200, subscribersList, "Subscribers fetched successfully")
+    );
+});
+
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+  // Get the subscriber id to find subscribed channels
+  const { subscriberId } = req.params;
+
+  // Validate it
+  if (!(subscriberId && isValidObjectId(subscriberId))) {
+    throw new ApiError(400, "Invalid channel id");
+  }
+
+  // Find the subscribed channel using aggregation pipeline
+  const subscribedChannelList = await Subscription.aggregate([
+    // Stage-1: Match the subscriberId
+    {
+      $match: {
+        subscriberId: new mongoose.Types.ObjectId(subscriberId),
+      },
+    },
+    // Stage-2: Find the subscribed channel to join with users collecion
+    {
+      $lookup: {
+        from: "users",
+        localField: "channel",
+        foreignField: "_id",
+        as: "subscribed",
+        pipeline: [
+          // Stage-2.1: Find the subscribed channel to join with subscription collections
+          {
+            $lookup: {
+              from: "subscriptions",
+              localField: "_id",
+              foreignField: "subscriber",
+              as: "subscribedChannels",
+            },
+          },
+          // Stage-2.2: Count the number of subscribed channels
+          {
+            $addFields: {
+              subscribedChannelCount: {
+                $size: "$subscribedChannels",
+              },
+            },
+          },
+        ],
+      },
+    },
+    // Stage-3: Deconstruct to an array field
+    {
+      $unwind: "$subscribed",
+    },
+    // Stage-4: Return neccessary details
+    {
+      $project: {
+        fullName: "$subscribed.fullName",
+        username: "$subscribed.username",
+        avtar: "$subscribed.avtar",
+        subscribedChannelCount: "$subscribed.subscribedChannelCount",
+      },
+    },
+  ]);
+
+  // Validate it
+  if (!subscribedChannelList) {
+    throw new ApiError(500, "Subscribed Channel list not found");
   }
 
   return res
@@ -84,10 +156,10 @@ const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     .json(
       new ApiResponse(
         200,
-        subscribersList[0],
-        "Subscribers fetched successfully"
+        subscribedChannelList,
+        "Subscribed channel list fetched successfully"
       )
     );
 });
 
-export { getUserChannelSubscribers };
+export { getUserChannelSubscribers, getSubscribedChannels };
