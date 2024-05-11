332e8bde29453533da26d45ecdfde34b5dd202ce
Dibyajyoti Sahoo <dibyajyotisahoo2504@gmail.com>
2024-05-11 11:39:45 +0530

    create like route for all like controller endpoint

---
+++ src/routes/like.routes.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {
+  getLikedVideos,
+  toggleCommentLike,
+  toggleTweetLike,
+  toggleVideoLike,
+} from "../controllers/like.controller.js";
+
+const router = Router();
+
+// Apply middleware for all subscriptions routes
+router.use(verifyJWT);
+
+router.route("/toggle/video/:videoId").post(toggleVideoLike);
+router.route("/toggle/comment/:commentId").post(toggleCommentLike);
+router.route("/toggle/tweet/:tweetId").post(toggleTweetLike);
+router.route("/videos").get(getLikedVideos);
+
+export default router;
